1175e6298a59dcf4d4d1d0255d0995b790ebd7f0
joneschean@gmail.com <a13675452>
2021-04-29 10:16:49 +0800

    換Form的範例程式

---
+++ CollectProgramShell-ChangeForm/CollectProgramShell/Form2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CollectProgramShell
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Program.SetNewForm(FORM_NAMES.eMAIN_MENU);
+        }
+
+        private void Form2_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form2_Move(object sender, EventArgs e)
+        {
+            Program.SyncLocation();
+        }
+    }
+}
---
+++ CollectProgramShell-ChangeForm/CollectProgramShell/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace CollectProgramShell
+{
+    enum FORM_NAMES:int
+    {
+        eMAIN_MENU,
+        eFORM2,
+    }
+    static class Program
+    {
+        static private Point oAppLocation;
+        static private Form oCurrentForm = null;
+        static private Dictionary<FORM_NAMES, Form> oProgramForms = new Dictionary<FORM_NAMES, Form>();
+        /// <summary>
+        /// 應用程式的主要進入點。
+        /// </summary>\
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            oProgramForms.Add(FORM_NAMES.eMAIN_MENU, new MainMenu());
+            oProgramForms.Add(FORM_NAMES.eFORM2, new Form2());
+
+            oCurrentForm = oProgramForms[FORM_NAMES.eMAIN_MENU];
+            oProgramForms[FORM_NAMES.eMAIN_MENU].Show();
+            oAppLocation = oProgramForms[FORM_NAMES.eMAIN_MENU].Location;
+
+            foreach (KeyValuePair<FORM_NAMES, Form> pair in oProgramForms)
+            {
+                if (!pair.Value.Equals(oCurrentForm))
+                {
+                    pair.Value.Opacity = 0;
+                    pair.Value.Show();
+                    pair.Value.Location = oAppLocation;
+                    pair.Value.Hide();
+                    pair.Value.Opacity = 255;
+                }
+            }
+            Application.Run();
+        }
+
+        static public void SyncLocation()
+        {
+            oAppLocation = oCurrentForm.Location;
+            foreach (KeyValuePair<FORM_NAMES, Form> pair in oProgramForms)
+            {
+                if (!pair.Value.Equals(oCurrentForm))
+                {
+                    pair.Value.Opacity = 0;
+                    pair.Value.Show();
+                    pair.Value.Location = oAppLocation;
+                    pair.Value.Hide();
+                    pair.Value.Opacity = 255;
+                }
+            }
+            //*/
+        }
+
+        static public void SetNewForm(FORM_NAMES eForm)
+        {
+            if(oProgramForms.ContainsKey(eForm))
+            {
+                if(oProgramForms[eForm] != null)
+                {
+                    oProgramForms[eForm].Show();
+                    oCurrentForm.Hide();
+                    oCurrentForm = oProgramForms[eForm];
+                }
+            }
+        }
+
+    }
+}
