4f99c2cae533a089182336f1b49829e310a7ffa3
joneschean@gmail.com <a13675452>
2021-05-27 13:28:31 +0800

    VB 射龍門

---
+++ WinFormsApp1VB/WinFormsApp1VB/Form1.Designer.vb
@@ -0,0 +1,165 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請勿使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.picLeft = New System.Windows.Forms.PictureBox()
+        Me.picRight = New System.Windows.Forms.PictureBox()
+        Me.picMiddle = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lblScoreCom = New System.Windows.Forms.Label()
+        Me.lblScorePlayer = New System.Windows.Forms.Label()
+        Me.lblGameFinish = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.btnRoundOne = New System.Windows.Forms.Button()
+        CType(Me.picLeft, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picRight, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picMiddle, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'picLeft
+        '
+        Me.picLeft.Location = New System.Drawing.Point(187, 88)
+        Me.picLeft.Name = "picLeft"
+        Me.picLeft.Size = New System.Drawing.Size(81, 124)
+        Me.picLeft.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.picLeft.TabIndex = 0
+        Me.picLeft.TabStop = False
+        '
+        'picRight
+        '
+        Me.picRight.Location = New System.Drawing.Point(515, 88)
+        Me.picRight.Name = "picRight"
+        Me.picRight.Size = New System.Drawing.Size(81, 124)
+        Me.picRight.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.picRight.TabIndex = 1
+        Me.picRight.TabStop = False
+        '
+        'picMiddle
+        '
+        Me.picMiddle.Location = New System.Drawing.Point(349, 260)
+        Me.picMiddle.Name = "picMiddle"
+        Me.picMiddle.Size = New System.Drawing.Size(81, 124)
+        Me.picMiddle.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.picMiddle.TabIndex = 0
+        Me.picMiddle.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("微軟正黑體", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Label1.Location = New System.Drawing.Point(13, 13)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(90, 24)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "電腦分數:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("微軟正黑體", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Label2.Location = New System.Drawing.Point(13, 37)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(90, 24)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "玩家分數:"
+        '
+        'lblScoreCom
+        '
+        Me.lblScoreCom.AutoSize = True
+        Me.lblScoreCom.Font = New System.Drawing.Font("微軟正黑體", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.lblScoreCom.Location = New System.Drawing.Point(99, 13)
+        Me.lblScoreCom.Name = "lblScoreCom"
+        Me.lblScoreCom.Size = New System.Drawing.Size(21, 24)
+        Me.lblScoreCom.TabIndex = 2
+        Me.lblScoreCom.Text = "0"
+        '
+        'lblScorePlayer
+        '
+        Me.lblScorePlayer.AutoSize = True
+        Me.lblScorePlayer.Font = New System.Drawing.Font("微軟正黑體", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.lblScorePlayer.Location = New System.Drawing.Point(99, 37)
+        Me.lblScorePlayer.Name = "lblScorePlayer"
+        Me.lblScorePlayer.Size = New System.Drawing.Size(21, 24)
+        Me.lblScorePlayer.TabIndex = 2
+        Me.lblScorePlayer.Text = "0"
+        '
+        'lblGameFinish
+        '
+        Me.lblGameFinish.AutoSize = True
+        Me.lblGameFinish.Font = New System.Drawing.Font("微軟正黑體", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.lblGameFinish.Location = New System.Drawing.Point(296, 13)
+        Me.lblGameFinish.Name = "lblGameFinish"
+        Me.lblGameFinish.Size = New System.Drawing.Size(90, 24)
+        Me.lblGameFinish.TabIndex = 2
+        Me.lblGameFinish.Text = "電腦分數:"
+        '
+        'Timer1
+        '
+        '
+        'btnRoundOne
+        '
+        Me.btnRoundOne.Font = New System.Drawing.Font("微軟正黑體", 14.25!)
+        Me.btnRoundOne.Location = New System.Drawing.Point(515, 342)
+        Me.btnRoundOne.Name = "btnRoundOne"
+        Me.btnRoundOne.Size = New System.Drawing.Size(115, 42)
+        Me.btnRoundOne.TabIndex = 3
+        Me.btnRoundOne.Text = "再來一局"
+        Me.btnRoundOne.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnRoundOne)
+        Me.Controls.Add(Me.lblScorePlayer)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.lblScoreCom)
+        Me.Controls.Add(Me.lblGameFinish)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.picRight)
+        Me.Controls.Add(Me.picMiddle)
+        Me.Controls.Add(Me.picLeft)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.picLeft, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picRight, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picMiddle, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents picLeft As PictureBox
+    Friend WithEvents picRight As PictureBox
+    Friend WithEvents picMiddle As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents lblScoreCom As Label
+    Friend WithEvents lblScorePlayer As Label
+    Friend WithEvents lblGameFinish As Label
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents btnRoundOne As Button
+End Class
---
+++ WinFormsApp1VB/WinFormsApp1VB/Form1.vb
@@ -0,0 +1,193 @@
+﻿Imports System.Threading
+
+Public Class Form1
+    Private oCardTable As Hashtable
+    Private oCardTableBack As Hashtable
+    Private leftNum As Integer
+    Private rightNum As Integer
+    Private iScoreCom As Integer = 0
+    Private iScorePlayer As Integer = 0
+    Private iSeed As Integer = 0
+    Private backCardImage As Image
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        backCardImage = Image.FromFile(".\\pic\\bicycle_backs.jpg")
+        Initial()
+        Start()
+    End Sub
+
+    Private Sub Initial()
+        oCardTable = New Hashtable
+        oCardTableBack = New Hashtable
+
+
+        For i = 1 To 13
+            oCardTable.Add(i, New ArrayList)
+            oCardTableBack.Add(i, New ArrayList)
+        Next
+
+        LoadCardPicture(oCardTable)
+
+        Reset()
+    End Sub
+
+    Private Sub Reset(Optional bSwap As Boolean = False)
+        Dim rnd As Random = New Random()
+        Dim tmpTable As Hashtable
+
+        iScoreCom = 0
+        iScorePlayer = 0
+        lblGameFinish.Text = ""
+        picLeft.Image = backCardImage
+        picRight.Image = backCardImage
+        picMiddle.Image = backCardImage
+        lblScoreCom.Text = iScoreCom
+        lblScorePlayer.Text = iScorePlayer
+        Timer1.Interval = 3000
+        iSeed = rnd.Next()
+        picMiddle.Enabled = True
+        btnRoundOne.Enabled = False
+        btnRoundOne.Visible = False
+
+        If bSwap Then
+            tmpTable = oCardTable
+            oCardTable = oCardTableBack
+            oCardTableBack = tmpTable
+            For i = 1 To 13
+                oCardTableBack.Add(i, New ArrayList)
+            Next
+        End If
+    End Sub
+
+    Private Sub LoadCardPicture(ByRef table As Hashtable)
+        Dim path = ""
+
+
+        For i = 1 To 13
+            Dim picList As ArrayList = table(i)
+            For j = 0 To 3
+                path = ".\\pic\\p" & ((i + j * 13)).ToString("D2") & ".jpg"
+                picList.Add(Image.FromFile(path))
+            Next
+        Next
+    End Sub
+
+    Private Sub Start()
+
+        SelectCard(picLeft, leftNum)
+        SelectCard(picRight, rightNum)
+
+    End Sub
+
+    Private Sub SelectCard(ByRef pic As PictureBox, ByRef val As Integer, Optional bRemoveSelect As Boolean = True)
+        Dim rnd As Random = New Random(iSeed)
+        Dim cards As ArrayList
+        Dim sn
+        Dim cardImg
+
+        Dim keys = oCardTable.Keys
+
+
+        sn = rnd.Next(oCardTable.Count)
+        Dim key = keys(sn)
+        val = key
+        cards = oCardTable(key)
+
+        sn = rnd.Next(cards.Count)
+        cardImg = cards(sn)
+
+        pic.Image = cardImg
+        If bRemoveSelect Then
+            cards.Remove(cardImg)
+            Dim tmpCardList = oCardTableBack(key)
+            tmpCardList.Add(cardImg)
+        End If
+
+        If cards.Count <= 0 Then
+            oCardTable.Remove(key)
+        End If
+        iSeed = rnd.Next()
+    End Sub
+
+    Private Sub picMiddle_Click(sender As Object, e As EventArgs) Handles picMiddle.Click
+        Dim rnd As Random = New Random
+        Dim middle As Integer
+
+
+        SelectCard(picMiddle, middle, False)
+
+        If leftNum < middle And rightNum > middle Then
+            iScorePlayer += 50
+            iScoreCom -= 50
+            lblGameFinish.Text = "勝 +50分"
+        ElseIf leftNum > middle And rightNum < middle Then
+            iScorePlayer += 50
+            iScoreCom -= 50
+            lblGameFinish.Text = "勝 +50分"
+        Else
+            iScorePlayer -= 50
+            iScoreCom += 50
+            lblGameFinish.Text = "負 -50分"
+        End If
+
+        lblScoreCom.Text = iScoreCom
+        lblScorePlayer.Text = iScorePlayer
+
+        Timer1.Start()
+
+
+    End Sub
+
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Dim remainCards As Integer
+        Dim keys = oCardTable.Keys
+
+        Timer1.Stop()
+
+        picMiddle.Image = backCardImage
+        remainCards = 0
+        For Each key In keys
+            Dim list As ArrayList = oCardTable(key)
+            remainCards += list.Count
+        Next
+
+        If remainCards > 2 Then
+            lblGameFinish.Text = ""
+            Start()
+        Else
+            GameFinish()
+
+        End If
+    End Sub
+
+    Private Sub GameFinish()
+        Dim keys
+        Dim list As ArrayList
+        Dim tmpList As ArrayList
+        Dim img As Image
+
+        keys = oCardTable.Keys
+        For Each key In keys
+            list = oCardTable(key)
+            tmpList = oCardTableBack(key)
+            For Each img In list
+                tmpList.Add(img)
+            Next
+            list.Clear()
+        Next
+        oCardTable.Clear()
+        picMiddle.Enabled = False
+        lblGameFinish.Text = "遊戲結束"
+        btnRoundOne.Enabled = True
+        btnRoundOne.Visible = True
+        picLeft.Image = backCardImage
+        picRight.Image = backCardImage
+    End Sub
+
+    Private Sub btnRoundOne_Click(sender As Object, e As EventArgs) Handles btnRoundOne.Click
+        Reset(True)
+        Start()
+    End Sub
+
+End Class
